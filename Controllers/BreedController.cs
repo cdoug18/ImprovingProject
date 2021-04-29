48403549061b357273c0c2479d7718852d1eb20f
CodyDouglas033@outlook.com <CodyDouglas033@outlook.com>
2021-04-29 14:35:07 -0500

    First Commit

---
+++ Controllers/BreedController.cs
@@ -0,0 +1,26 @@
+﻿using ImprovingProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ImprovingProject.Controllers
+{
+    public class BreedController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            Breed _breeds = new Breed();
+
+            using (var _httpClient = new HttpClient())
+            {
+                using var _request = await _httpClient.GetAsync("https://dog.ceo/api/breeds/list");
+                string _response = await _request.Content.ReadAsStringAsync();
+                _breeds = JsonConvert.DeserializeObject<Breed>(_response);
+            }
+
+            return View(_breeds);            
+        }
+    }
+}
---
+++ Models/Breed.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ImprovingProject.Models
+{
+    public class Breed
+    {
+        public List<string> Message { get; set; }
+        public string Status { get; set; }
+    }
+}
